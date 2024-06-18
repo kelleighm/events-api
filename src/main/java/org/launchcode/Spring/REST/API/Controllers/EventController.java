8b67ebff14562e6c8f629e0b1c4559af35821730
kelleighm <kelleighmmartin@gmail.com>
2024-06-17 20:14:50 -0500

    FINISHED

--- src/main/java/org/launchcode/Spring/REST/API/Controllers/EventController.java
+++ src/main/java/org/launchcode/Spring/REST/API/Controllers/EventController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//finished studio
+
+
 @RestController
 @RequestMapping(value = "/events")
 public class EventController {
